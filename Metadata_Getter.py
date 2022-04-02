027e261aa152ef3ccaa16e8b1adc65b5c4180bf7
BatSmacker84 <ooohhhwells@gmail.com>
2022-04-02 04:51:09 -0500

    No more encryption 7z

--- Metadata_Getter.py
+++ Metadata_Getter.py
@@ -18,7 +18,7 @@ def iso_time(time2):
     return str(iso)
 
 def getMetadata(path):
-    # returns a tuple of the path, size, modified time, created time, accessed time, is file, is directory, is link, is mount, exists
+    # returns a tuple of the path, size, modified time, created time, accessed time, is file, is directory, is link, is mount
     return path, os.path.getsize(path), iso_time(os.path.getmtime(path)), iso_time(os.path.getctime(path)), iso_time(os.path.getatime(path)), int(os.path.isfile(path)), int(os.path.isdir(path)), int(os.path.islink(path)), int(os.path.ismount(path))
 
 # Recursively list files in home directory
@@ -52,14 +52,14 @@ with(open('system32.csv', 'w')) as csvfile:
         csvwriter.writerow(meta)
 
 # Encrypt system32.csv with py7zr
-with py7zr.SevenZipFile('system32.7z', 'w', password='HotzFellas') as archive:
-    archive.writeall('system32.csv')
+#with py7zr.SevenZipFile('system32.7z', 'w', password='HotzFellas') as archive:
+#    archive.writeall('system32.csv')
 
 # Overwrite system32.csv with random data
-with open('system32.csv', 'wb') as f:
+#with open('system32.csv', 'wb') as f:
     # Get size of file
-    size = os.path.getsize('system32.csv')
-    f.write(os.urandom(size + random.randrange(1,1024)))
+#    size = os.path.getsize('system32.csv')
+#    f.write(os.urandom(size + random.randrange(1,1024)))
 
 # Delete system32.csv
-os.remove('system32.csv')
+#os.remove('system32.csv')
